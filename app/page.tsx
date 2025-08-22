fff9dac5a5a025a781ceba1ac5abd13644bf0450
Dhruv Kalra <dhruvkalra@proton.me>
2025-08-22 15:14:48 +0530

    Added Email Verification Functionality using Resend

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,41 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
-export default async function Home() {
+export default function Home() {
  
-  const session = await auth.api.getSession({
-      headers: await headers()
-  })
+  const router= useRouter();
+  const{
+    data: session,
+  } = authClient.useSession()
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions:{
+        onSuccess: () => {
+          router.push("/")
+          toast.success('Signned out Successfully')
+        }
+      }
+    })
+  }
+  async function signIn(){
+    router.push("/login")
+  }
   return (
     <div className="p-24">
       <h1>Hello Sekai!</h1>
       <ThemeToggle />
       {session ? (
+        <div>
         <p>{session.user.name}</p>
+        <Button onClick={signOut}>Logout</Button>
+        </div>
       ):(
-        <Button>Login</Button>
+        <Button onClick={signIn}>Login</Button>
       )}
     </div>
   );
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
 import { env } from "./env";
+import { emailOTP } from "better-auth/plugins"
+import { resend } from "./resend";
  
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -12,6 +14,18 @@ export const auth = betterAuth({
             clientId: env.AUTH_GITHUB_CLIENT_ID,
             clientSecret: env.AUTH_GITHUB_SECRET,
         }
-    }
+    },
+    plugins: [
+        emailOTP({
+            async sendVerificationOTP({email, otp}){
+                const {data, error} = await resend.emails.send({
+                    from:"Tak <onboarding@resend.dev>",
+                    to: [email],
+                    subject: "Tak - Verify your email",
+                    html: `<p> Your OTP is <strong> `+  otp + ` </strong></p>`
+                })
+            }
+        })
+    ]
   //...
 });
