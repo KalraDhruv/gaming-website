0bc52cd48d6c6a861ee8520c291f7882d0e1be7e
Dhruv Kalra <dhruvkalra@proton.me>
2025-08-22 10:59:32 +0530

    Added Login Page Frontend

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,33 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { ReactNode } from "react";
+import  Crown from '@/public/crown.svg'
+
+export default function AuthLayout({children}:{children:ReactNode}){
+    return (
+    <div className="relative flex min-h-svh flex-col items-center justify-center">
+        <Link href="/" className={buttonVariants({
+            variant: "outline",
+            className: "absolute top-4 left-4",
+        })}>
+            <ArrowLeft className="size-4"/>
+            Back
+       </Link>
+        <div className="flex w-full max-w-sm flex-col gap-6">
+            <Link href="/" className="flex justify-center font-medium gap-2 items-center self-center flex-row">
+                <img src={'crown.svg'} alt="Crown"
+                style={{maxWidth:35, maxHeight:35}}/>
+                Tak
+            </Link>
+           {children} 
+           <div className="text-balance text-center text-xs text-muted-foreground">
+            By clicking continue, you agree to our <span className="hover:text-primary hover:underline"> Terms of service </span> 
+            {" "}
+            and <span className="hover:text-primary hover:underline">Privacy Policy</span>.
+           </div>
+        </div>
+    </div>
+    )
+    
+}
